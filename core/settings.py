84415976258de59a6f3136d4513f791f4bc90d0a
faycal-dev <m.elmogherbi@esi-sba.dz>
2022-11-10 15:20:56 +0100

    adding static files

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
@@ -125,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # the place where to store images
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
